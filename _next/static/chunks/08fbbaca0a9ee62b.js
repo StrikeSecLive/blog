8def3757732f7316322017b78ce30658cef6a8bc
StrikeSecLive <StrikeSecLive@users.noreply.github.com>
2025-11-26 12:14:00 +0000

    deploy: 30a68db635967ce6916ef67060ce7adcd87b96ed

---
+++ _next/static/chunks/08fbbaca0a9ee62b.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/", [
+  "static/chunks/547d4e726b8f06ec.js",
+  "static/chunks/5b133b29957c2e1b.js",
+  "static/chunks/turbopack-835a10ea6e4cc540.js"
+])
---
+++ _next/static/xbQXjxEbpLox5wMu3d8X_/_buildManifest.js
@@ -0,0 +1,23 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/08fbbaca0a9ee62b.js"
+  ],
+  "/_error": [
+    "static/chunks/516c810539b1d532.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/blog//_next/:path+",
+        "destination": "/blog/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
