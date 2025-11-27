43efecb1df3373ec12bdd08a481f6f8478457efc
StrikeSecLive <StrikeSecLive@gmail.com>
2025-11-27 13:26:11 +0000

    Update MDXComponents.js

--- components/MDXComponents.js
+++ components/MDXComponents.js
@@ -1,26 +1,26 @@
-import { MDXRemote } from 'next-mdx-remote';
-import MDXComponents from '../components/MDXComponents';
 
-export default function BlogLayout({ mdxSource, children, frontMatter }) {
-  return (
-    <article className="prose mx-auto">
-      {/* Optional: render title/date/tags from frontMatter */}
-      {frontMatter?.title && <h1>{frontMatter.title}</h1>}
-      {frontMatter?.date && (
-        <p className="text-sm text-gray-500">
-          {new Date(frontMatter.date).toLocaleDateString(undefined, {
-            year: 'numeric',
-            month: 'short',
-            day: 'numeric',
-          })}
-        </p>
-      )}
+import React from 'react';
+import Link from 'next/link';
+import CodeBlock from './CodeBlock';
 
-      {/* Render MDX content */}
-      <MDXRemote {...mdxSource} components={MDXComponents} />
-
-      {/* If the page passes extra children, render them after */}
-      {children}
-    </article>
+const A = (props) => {
+  const href = props.href ?? '';
+  const isExternal = typeof href === 'string' && /^https?:\/\//.test(href);
+  return isExternal ? (
+    <a href={href} target="_blank" rel="noopener noreferrer">{props.children}</a>
+  ) : (
+    <Link href={href}>{props.children}</Link>
   );
-}
+};
+
+const Pre = (props) => <pre {...props} />;
+const Img = (props) => <img {...props} />;
+
+const MDXComponents = {
+  a: A,
+  pre: Pre,
+  code: (props) => <CodeBlock {...props} />,
+  img: Img,
+};
+
+export default MDXComponents;
