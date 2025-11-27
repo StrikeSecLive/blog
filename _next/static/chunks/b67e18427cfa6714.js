0f90a93b465699fb6b55207848dc43f8308135c2
StrikeSecLive <StrikeSecLive@users.noreply.github.com>
2025-11-27 09:59:44 +0000

    deploy: b35a06ad336d27215467120fe638a68618305c1d

--- _next/static/chunks/b67e18427cfa6714.js
+++ _next/static/chunks/b67e18427cfa6714.js
@@ -2,6 +2,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/c86faf1fe89c2f6f.js",
   "static/chunks/9cd3de017dc9873a.js",
   "static/chunks/6d68a81d5940fc1a.js",
-  "static/chunks/4e8e4cd48b3bc7db.css",
-  "static/chunks/turbopack-8abd270ffe1d7a5d.js"
+  "static/chunks/bdbcf5eced5825f5.css",
+  "static/chunks/turbopack-b8f24f48ec43ebc9.js"
 ])
