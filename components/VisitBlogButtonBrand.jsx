7b56ffe08172e9d7fa47c5030fa16755cb7636b8
StrikeSecLive <StrikeSecLive@gmail.com>
2025-11-27 16:24:30 +0000

    Update VisitBlogButtonBrand.jsx

--- components/VisitBlogButtonBrand.jsx
+++ components/VisitBlogButtonBrand.jsx
@@ -1,11 +1,9 @@
 import Link from 'next/link';
+import '../styles/button.css';
 
 export default function VisitBlogLink() {
   return (
-    <Link
-      href="/blog"
-      className="inline-flex items-center justify-center px-6 py-3 text-lg font-semibold text-[#2b3742] bg-[#a5f63a] rounded-lg shadow-md hover:bg-[#8cd52f] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#a5f63a] transition-colors duration-300 no-underline"
-    >
+    <Link href="/blog">
       Visit Blog
     </Link>
   );
