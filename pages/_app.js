c8013c4eb2d6e03bf421b7c7c9d682df782397a3
StrikeSecLive <StrikeSecLive@gmail.com>
2025-11-27 11:02:36 +0000

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,8 @@ import Layout from '../components/Layout';
 import { MDXProvider } from '@mdx-js/react';
 import CodeBlock from '../components/CodeBlock';
 
-// Map <pre> to our CodeBlock so Prism/rehype markup stays intact
 const components = {
-  pre: (props) => <CodeBlock {...props} />,
+  pre: (props) => <CodeBlock {...props} />, // ✅ Wrap <pre> so Prism markup stays intact
 };
 
 export default function App({ Component, pageProps }) {
