5893eed7cbb431ab121f34e2f02717aa5c7ce4fb
StrikeSecLive <StrikeSecLive@gmail.com>
2025-11-27 13:09:42 +0000

    Update MDXComponents.js

--- components/MDXComponents.js
+++ components/MDXComponents.js
@@ -1,24 +1,19 @@
-
-// components/MDXComponents.js
 import React from 'react';
 import Link from 'next/link';
 import CodeBlock from './CodeBlock';
 
+// Safe anchor: external links use <a>, internal use <Link>
 const A = (props) => {
   const href = props.href ?? '';
-  const isExternal = typeof href === 'string' && href.startsWith('http');
-  if (isExternal) {
-    return (
-      <a href={href} target="_blank" rel="noopener noreferrer" {...props}>
-        {props.children}
-      </a>
-    );
-  }
-  return <Link href={href} {...props}>{props.children}</Link>;
+  const isExternal = typeof href === 'string' && /^https?:\/\//.test(href);
+  return isExternal ? (
+    <a href={href} target="_blank" rel="noopener noreferrer">{props.children}</a>
+  ) : (
+    <Link href={href}>{props.children}</Link>
+  );
 };
 
-const Pre = (props) => <CodeBlock {...props} />;
-
+const Pre = (props) => <pre {...props} />;
 const Img = (props) => <img {...props} />;
 
 const MDXComponents = {
