6a2128f50bf587d5b4c9fc74a2f2126b722da837
StrikeSecLive <StrikeSecLive@gmail.com>
2025-11-27 16:28:32 +0000

    Update VisitBlogButtonBrand.jsx

--- components/VisitBlogButtonBrand.jsx
+++ components/VisitBlogButtonBrand.jsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import styles from '@/styles/button.module.css';
+import styles from '../styles/button.module.css';
 
 export default function VisitBlogButton() {
   return (
