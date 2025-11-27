d1ee3118086048be63bf9595dc2020b97792842e
StrikeSecLive <StrikeSecLive@gmail.com>
2025-11-27 12:24:35 +0000

    Add files via upload

---
+++ pages/blog/BlogLayout.jsx
@@ -0,0 +1,15 @@
+import { MDXRemote } from 'next-mdx-remote';
+import MDXComponents from '@/components/MDXComponents';
+
+export default function BlogLayout({ mdxSource }) {
+  return (
+    <main className="min-h-screen bg-gray-900 text-gray-100">
+      <div className="mx-auto max-w-4xl px-6 py-10">
+        {/* Blog content wrapper */}
+        <article className="prose prose-invert max-w-none dark:prose-invert">
+          <MDXRemote {...mdxSource} components={MDXComponents} />
+        </article>
+      </div>
+    </main>
+  );
+}
