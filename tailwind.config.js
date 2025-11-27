7f750d432a835cfb448a78863dc618505b7fcf5d
StrikeSecLive <StrikeSecLive@gmail.com>
2025-11-27 12:25:42 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 module.exports = {
-  content: ['./pages/**/*.{js,jsx,md,mdx}', './components/**/*.{js,jsx}', './content/**/*.{md,mdx}','./styles/**/*.css',],
+  content: [
+    './pages/**/*.{js,jsx,md,mdx}',
+    './components/**/*.{js,jsx}',
+    './content/**/*.{md,mdx}',
+    './styles/**/*.{css}',
+  ],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
         navy: '#2C3848',
         accent: '#a5f63a',
       },
-    },
-  },  
-  // tailwind.config.js
-  darkMode: 'class',
-  plugins: [require('@tailwindcss/typography')],
-  theme: {
-    extend: {
       typography: (theme) => ({
         DEFAULT: {
           css: {
@@ -21,14 +20,19 @@ module.exports = {
               color: theme('colors.gray.100'),
               borderRadius: '0.5rem',
               padding: '1rem',
+              overflowX: 'auto',
             },
             code: {
               color: theme('colors.accent'),
               fontWeight: '500',
+              backgroundColor: theme('colors.gray.800'),
+              padding: '0.2em 0.4em',
+              borderRadius: '4px',
             },
           },
         },
       }),
     },
   },
+  plugins: [require('@tailwindcss/typography')],
 };
