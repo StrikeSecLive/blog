ba4e7d1af4bedb3a6bca1e01589a542c942f52e8
StrikeSecLive <StrikeSecLive@users.noreply.github.com>
2025-11-27 13:38:13 +0000

    deploy: 2c466fbe76e67e4b8e9562dd385e8939980523c4

--- _next/static/chunks/1c62aa4d984d5f0b.js
+++ _next/static/chunks/1c62aa4d984d5f0b.js
@@ -1,6 +1,6 @@
 __turbopack_load_page_chunks__("/blog/[slug]", [
-  "static/chunks/ffc47ee04b5356dd.js",
+  "static/chunks/d6f1ac54654dab38.js",
   "static/chunks/28d87c9b8ad3db1f.js",
   "static/chunks/f86a717450cd75fa.js",
-  "static/chunks/turbopack-2642ab3f40707de0.js"
+  "static/chunks/turbopack-132804537c29cfd0.js"
 ])
--- _next/static/jdxQp6_EY4pKD3aOAbqQa/_buildManifest.js
+++ _next/static/jdxQp6_EY4pKD3aOAbqQa/_buildManifest.js
@@ -9,7 +9,7 @@ self.__BUILD_MANIFEST = {
     "static/chunks/9c2d68f4dcd5e71f.js"
   ],
   "/blog/[slug]": [
-    "static/chunks/6824866acc848c20.js"
+    "static/chunks/1c62aa4d984d5f0b.js"
   ],
   "__rewrites": {
     "afterFiles": [],
