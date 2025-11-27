a26850df71c04f960ef26c4e48b827bcb35ff073
StrikeSecLive <StrikeSecLive@gmail.com>
2025-11-27 11:46:50 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
-const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ });
-
-module.exports = withMDX({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   output: 'export',
   trailingSlash: true,
-  images: { unoptimized: true },  
-  options: {
-    providerImportSource: '@mdx-js/react', // Enables MDXProvider
-  },
-});
+  images: { unoptimized: true },
+};
+
+module.exports = nextConfig;
