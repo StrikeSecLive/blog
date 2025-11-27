2c466fbe76e67e4b8e9562dd385e8939980523c4
StrikeSecLive <StrikeSecLive@gmail.com>
2025-11-27 13:37:42 +0000

    Update BlogLayout.jsx

--- layouts/BlogLayout.jsx
+++ layouts/BlogLayout.jsx
@@ -6,7 +6,7 @@ export default function BlogLayout({ mdxSource }) {
     <main className="min-h-screen bg-gray-900 text-gray-100">
       <div className="mx-auto max-w-4xl px-6 py-10">
         {/* Blog content wrapper */}
-        <article className="prose prose-invert max-w-none dark:prose-invert">
+        <article className="prose mx-auto px-4 sm:px-6 lg:px-8 max-w-3xl dark:prose-invert">
           <MDXRemote {...mdxSource} components={MDXComponents} />
         </article>
       </div>
