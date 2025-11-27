4104c80b664c213321710e3789e1c98f1511003b
StrikeSecLive <StrikeSecLive@gmail.com>
2025-11-27 12:27:32 +0000

    Update BlogLayout.jsx

--- pages/blog/BlogLayout.jsx
+++ pages/blog/BlogLayout.jsx
@@ -1,5 +1,5 @@
 import { MDXRemote } from 'next-mdx-remote';
-import MDXComponents from '@/components/MDXComponents';
+import MDXComponents from '../../components/MDXComponents';
 
 export default function BlogLayout({ mdxSource }) {
   return (
