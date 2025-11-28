dcae642baca61683cf9980be8c08da8e3ad157bd
StrikeSecLive <StrikeSecLive@users.noreply.github.com>
2025-11-28 11:21:07 +0000

    deploy: 7f2389982554500f253a75b364206422c524dd5e

--- _next/static/AVud-TELwOjSVFpaI7uBz/_buildManifest.js
+++ _next/static/AVud-TELwOjSVFpaI7uBz/_buildManifest.js
@@ -9,7 +9,7 @@ self.__BUILD_MANIFEST = {
     "static/chunks/80a76cc43f5496e0.js"
   ],
   "/blog/[slug]": [
-    "static/chunks/abf9878e59702881.js"
+    "static/chunks/e7238bf589adbdaa.js"
   ],
   "__rewrites": {
     "afterFiles": [],
--- _next/static/chunks/e7238bf589adbdaa.js
+++ _next/static/chunks/e7238bf589adbdaa.js
@@ -1,6 +1,6 @@
 __turbopack_load_page_chunks__("/blog/[slug]", [
-  "static/chunks/bd9f58c6e38ccd38.js",
+  "static/chunks/8905452248d1c53f.js",
   "static/chunks/28d87c9b8ad3db1f.js",
   "static/chunks/f86a717450cd75fa.js",
-  "static/chunks/turbopack-0f0aca410c33c751.js"
+  "static/chunks/turbopack-1d5c1e8a0bfc148d.js"
 ])
